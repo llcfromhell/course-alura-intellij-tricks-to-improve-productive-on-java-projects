1bdb0f5e84dbaa3ca14b2eabbe2811432f4199d7
Lourenco Cunha <lourenco.dacunha@axispoint.com>
2018-01-04 19:47:26 -0200

    lesson 06

--- intellij-idea-aula-3/src/br/com/alura/bytebank/Principal.java
+++ intellij-idea-aula-3/src/br/com/alura/bytebank/Principal.java
@@ -1,10 +1,37 @@
 package br.com.alura.bytebank;
 
+import br.com.alura.bytebank.io.LeitorCSV;
+import br.com.alura.bytebank.model.Pagamento;
 import br.com.alura.bytebank.visualizador.OpcoesPagamento;
 
+import java.io.IOException;
+import java.util.List;
+
 public class Principal {
-    public static void main(String[] args) {
-        new OpcoesPagamento().mensagemDeBoasVindas();
+    public static void main(String[] args) throws IOException {
+
+//        new OpcoesPagamento().mensagemDeBoasVindas();
+
+        LeitorCSV leitorCSV = new LeitorCSV();
+//
+//        List<Pagamento> pagamentos = leitorCSV.ler(3);
+//
+//        for (Pagamento pagamento : pagamentos) {
+//            System.out.println(pagamento);
+//        }
+
+        leitorCSV.ler(1).stream()
+                .map(Principal::applyAcrescimo)
+                .forEach(System.out::println);
     }
 
+
+    private static Pagamento applyAcrescimo(Pagamento p) {
+        if (p.getValor() > 100) {
+            double valorBruto = p.getValor();
+            double valorComAcrescimo = valorBruto * 1.1;
+            p.setValor(valorComAcrescimo);
+        }
+        return p;
+    }
 }
