6f09f3f0dfd3ecbb8bb7d42e25fb18a79d30b697
Lourenco Cunha <lourenco.dacunha@axispoint.com>
2018-01-04 11:20:49 -0200

    aula 05

---
+++ intellij-idea-aula-3/src/br/com/alura/bytebank/io/teste/LeitorCSVTest.java
@@ -0,0 +1,33 @@
+package br.com.alura.bytebank.io.teste;
+
+import br.com.alura.bytebank.io.LeitorCSV;
+import br.com.alura.bytebank.model.Pagamento;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class LeitorCSVTest {
+
+    @Test
+    void pagamentosDeAbril() throws IOException {
+        LeitorCSV leitor = new LeitorCSV();
+        List<Pagamento> pagamentos = leitor.ler(1);
+        assertEquals(5, pagamentos.size());
+    }
+    @Test
+    void pagamentosDeMaio() throws IOException {
+        LeitorCSV leitor = new LeitorCSV();
+        List<Pagamento> pagamentos = leitor.ler(2);
+        assertEquals(5, pagamentos.size());
+    }
+    @Test
+    void pagamentosDeJunho() throws IOException {
+        LeitorCSV leitor = new LeitorCSV();
+        List<Pagamento> pagamentos = leitor.ler(3);
+        assertEquals(5, pagamentos.size());
+    }
+
+}
